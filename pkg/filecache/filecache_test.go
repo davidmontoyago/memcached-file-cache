d74b6dd09df4751c10677a6dc430c2769169f1c7
David Montoya <davidmontoyago@gmail.com>
2020-02-12 19:02:23 -0600

    fetch file parts by key and assemble

--- pkg/filecache/filecache_test.go
+++ pkg/filecache/filecache_test.go
@@ -1,6 +1,7 @@
 package filecache
 
 import (
+	"bytes"
 	"fmt"
 	"io/ioutil"
 	"os"
@@ -17,6 +18,11 @@ type mockMemcachedClient struct {
 func (c *mockMemcachedClient) Set(item *memcache.Item) error {
 	c.storedItems[item.Key] = item.Value
 	return nil
+}
+
+func (c *mockMemcachedClient) Get(key string) (item *memcache.Item, err error) {
+	val, _ := c.storedItems[key]
+	return &memcache.Item{Value: val}, nil
 }
 
 func TestPutStoresFileKeyWithChunksKeysAsValue(t *testing.T) {
@@ -48,5 +54,28 @@ func TestPutStoresFileKeyWithChunksKeysAsValue(t *testing.T) {
 		if chunkKey != expectedKey {
 			t.Errorf("expected file chunk key to be %s but got %s", expectedKey, chunkKey)
 		}
+	}
+}
+
+func TestGetFetchesChunksAndAssemblesFile(t *testing.T) {
+	memcached := &mockMemcachedClient{storedItems: make(map[string][]byte)}
+	fileCache := NewFileCache(memcached)
+
+	f, err := os.Open("../chunker/fixture/file.dat")
+	if err != nil {
+		t.Error(err)
+	}
+	file, err := ioutil.ReadAll(f)
+	if err != nil {
+		t.Error(err)
+	}
+	fileCache.Put(file)
+
+	assembledFile, err := fileCache.Get("91388263e7c545ebea3952fb2637dffa")
+	if err != nil {
+		t.Error(err)
+	}
+	if !bytes.Equal(file, assembledFile) {
+		t.Errorf("expected assembled file to contain same bytes as fixture but they differ")
 	}
 }
--- pkg/filecache/memcachedclient.go
+++ pkg/filecache/memcachedclient.go
@@ -4,4 +4,6 @@ import "github.com/bradfitz/gomemcache/memcache"
 
 type memcachedClient interface {
 	Set(item *memcache.Item) error
+
+	Get(key string) (item *memcache.Item, err error)
 }
