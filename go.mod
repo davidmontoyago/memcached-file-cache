d7440f9f00dc779e1b0dd02f8c89e9aa6ec29601
David Montoya <davidmontoyago@gmail.com>
2020-02-13 13:25:32 -0600

    check file max size & wrap errors

--- go.mod
+++ go.mod
@@ -5,5 +5,6 @@ go 1.13
 require (
 	github.com/bradfitz/gomemcache v0.0.0-20190913173617-a41fca850d0b
 	github.com/gorilla/mux v1.7.4
+	github.com/pkg/errors v0.9.1
 	github.com/urfave/cli/v2 v2.1.1
 )
--- pkg/filecache/filecache.go
+++ pkg/filecache/filecache.go
@@ -6,7 +6,11 @@ import (
 
 	"github.com/bradfitz/gomemcache/memcache"
 	"github.com/davidmontoyago/interview-davidmontoyago-d660952eff664d8bac96c9124d7f8582/pkg/chunker"
+	"github.com/pkg/errors"
 )
+
+// MaxFileSize 50MB
+const MaxFileSize = 52428800
 
 // FileCache interfaces with cache client and chunker apis for spliting and storing/fetching files
 type FileCache struct {
@@ -22,9 +26,12 @@ func New(memcache memcachedClient) *FileCache {
 
 // Put splits and places a file in the cache
 func (f *FileCache) Put(file []byte) (string, error) {
+	if err := checkFileSize(file); err != nil {
+		return "", err
+	}
 	chunkedFile := chunker.NewFromFile(file)
-	totalChunks := len(chunkedFile.Chunks())
 
+	totalChunks := len(chunkedFile.Chunks())
 	var sb strings.Builder
 	fileChunksByKey := make(map[string]*chunker.Chunk, totalChunks)
 	for count, chunk := range chunkedFile.Chunks() {
@@ -36,20 +43,28 @@ func (f *FileCache) Put(file []byte) (string, error) {
 	keys := strings.TrimSuffix(sb.String(), ",")
 
 	if err := f.putFileKeys(chunkedFile.Checksum(), keys); err != nil {
-		return "", err
+		return "", errors.Wrap(err, "failed to put file keys")
 	}
 	if err := f.putFileChunks(fileChunksByKey); err != nil {
 		// TODO remove file keys
-		return "", err
+		return "", errors.Wrap(err, "failed to put file's chunks")
 	}
 	return chunkedFile.Checksum(), nil
+}
+
+func checkFileSize(file []byte) error {
+	fileSize := len(file)
+	if fileSize > MaxFileSize {
+		return fmt.Errorf("file size %d exceeds the max allowed %d by %d bytes", fileSize, MaxFileSize, fileSize-MaxFileSize)
+	}
+	return nil
 }
 
 // Get fetches all file parts and returns the assembled file
 func (f *FileCache) Get(checksum string) ([]byte, error) {
 	fileKeys, err := f.memcache.Get(checksum)
 	if err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to get file keys")
 	}
 
 	var parts []*chunker.Chunk
@@ -57,14 +72,14 @@ func (f *FileCache) Get(checksum string) ([]byte, error) {
 	for _, chunkKey := range strings.Split(chunksKeys, ",") {
 		fileChunk, err := f.memcache.Get(chunkKey)
 		if err != nil {
-			return nil, err
+			return nil, errors.Wrap(err, "failed to get file chunk")
 		}
 		parts = append(parts, chunker.NewChunk(fileChunk.Value))
 	}
 
 	chunkedFile := chunker.NewFromChunks(parts)
 	if err := chunkedFile.Validate(checksum); err != nil {
-		return nil, err
+		return nil, errors.Wrap(err, "failed to validate file")
 	}
 	return chunkedFile.File(), nil
 }
